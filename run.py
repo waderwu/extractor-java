696dfa4385651b6b49c32b33f45852ee98850c7a
m0d9 <m0d9@m0d9.me>
2022-11-25 15:34:52 +0800

    repair oxs codeql_java_home

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 import glob
 import argparse
-
+import platform
 
 class Extract:
     def __init__(self, db, srcroot, lib, libdir):
@@ -18,22 +18,24 @@ class Extract:
             for _dir in libdir:
                 for lib in glob.glob(f"{_dir}/**/*.jar", recursive=True):
                     self.libs.append(lib)
-    
+
     def init_database(self):
-        p = subprocess.run(["codeql", "database", "init", self.dbname,  "-l",  "java", "--source-root", self.srcroot])
+        p = subprocess.run(["codeql", "database", "init", self.dbname, "-l", "java", "--source-root", self.srcroot])
         if p.returncode == 0:
             self.dbpath = os.path.realpath(self.dbname)
             print(f"[*extract_log*] dbpath : {self.dbpath}")
         else:
             sys.exit(1)
 
-    
     def init_env(self):
         codeql_path = subprocess.check_output(["which", "codeql"]).decode()
         codeql_home = os.path.dirname(codeql_path)
         self.codeql_home = codeql_home
         print(f"[*extract_log*] codeql_home : {codeql_home}")
-        codeql_java_home = glob.glob(f"{codeql_home}/tools/**/java")[0]
+        s = platform.system().lower()
+        s = 'osx' if s == 'darwin' else s
+        # print(f"{codeql_home}/tools/{s}*/java")
+        codeql_java_home = glob.glob(f"{codeql_home}/tools/{s}*/java")[0]
         self.codeql_java_home = codeql_java_home
         print(f"[*extract_log*] codeql_java_home : {codeql_java_home}")
         env = {
@@ -60,18 +62,21 @@ class Extract:
                 for lib in self.libs:
                     libstr = libstr + lib + ":"
                 f.write(libstr + "\n")
-            
-            for javafile in javafiles:
-                #if "test" not in javafile:
-                f.write(javafile + "\n")
 
+            for javafile in javafiles:
+                # if "test" not in javafile:
+                f.write(javafile + "\n")
 
     def generate_trap(self):
         env = self.init_env()
-        p = subprocess.run([f"{self.codeql_java_home}/bin/java", "-Xmx1024M", "-Xms256M", "-cp", f"{self.codeql_home}/java/tools/semmle-extractor-java.jar", "com.semmle.extractor.java.JavaExtractor", "--javac-args", f"@@@{self.dbpath}/log/javac.args"], env=env)
-    
+        p = subprocess.run([f"{self.codeql_java_home}/bin/java", "-Xmx1024M", "-Xms256M", "-cp",
+                            f"{self.codeql_home}/java/tools/semmle-extractor-java.jar",
+                            "com.semmle.extractor.java.JavaExtractor", "--javac-args",
+                            f"@@@{self.dbpath}/log/javac.args"], env=env)
+
     def import_trap(self):
-        p = subprocess.run(["codeql", "dataset", "import", f"{self.dbpath}/db-java", f"{self.dbpath}/trap", "-S", f"{self.codeql_home}/java/semmlecode.dbscheme"])
+        p = subprocess.run(["codeql", "dataset", "import", f"{self.dbpath}/db-java", f"{self.dbpath}/trap", "-S",
+                            f"{self.codeql_home}/java/semmlecode.dbscheme"])
 
     def finalize(self):
         p = subprocess.run(["codeql", "database", "finalize", self.dbpath])
@@ -94,7 +99,7 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         parser.print_help()
         sys.exit()
-    
+
     args = parser.parse_args()
     print(args)
     print(args.db)
